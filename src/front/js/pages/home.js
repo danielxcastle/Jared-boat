157afb63cb7167c7f95cd3e8b61cdbec0796e273
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-11-20 04:51:32 +0000

    styling with home container image

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,13 +9,13 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-2 home-page" style={{ backgroundImage: `url(${ocean})` }}>
-		  <h1>Welcome! Your journey awaits!</h1>
+		<div className="text-center mt-2 home-page" style={{ backgroundImage: `url(${ocean})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+		  <h1 className="home-header">Welcome! Your journey awaits!</h1>
 	
 		  <img className="home-image" src={boat} />
-		  <h5>
+		  <div className="home-content">
 			We are located in San Pedro, California. Our Captain Jared has over 10 years of experience in Sportfishing and would like to invite you to join us aboard the vessel.
-		  </h5>
+		  </div>
 		</div>
 	  );
 	};
