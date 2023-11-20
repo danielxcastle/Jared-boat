dd06b0fb5b66094a08f8b0fda05547c99a9a450d
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-11-20 04:19:15 +0000

    done for now

---
+++ src/front/js/component/instagramfeed.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const InstagramFeed = () => {
+    return (
+        <script type="text/javascript" src="https://www.juicer.io/embed/danielxcastle/embed-code.js" async defer></script>
+    )
+}
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,20 +5,29 @@ import navlogo from "../../img/navlogo.png";
 import navbackground from "../../img/navbackground.png";
 
 export const Navbar = () => {
-  return (
-    <nav className="navbar navbar-custom" style={{ backgroundImage: `url(${navbackground})`, height: '320px' }}>
-      <div className="container d-flex justify-content-align-items-center">
-        <img
-          src={navlogo}
-          alt="Dawn to Dusk Sportfishing"
-          className="navbar-logo"
-        />
+	return (
+		<nav className="navbar navbar-custom" style={{ backgroundImage: `url(${navbackground})`, height: '320px' }}>
+			<div className="container">
+				<div className="row">
+					<div className="col-3 top-left">
+						<img
+							src={navlogo}
+							alt="Dawn to Dusk Sportfishing"
+							className="navbar-logo"
+						/>
+					</div>
+					<div className="col-6">
+						
+					</div>
+					<div className="col-3 top-right">
+						<div className="nav-buttons">
+							<Phone />
+							<Email />
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
 
-        <div className="nav-buttons">
-          <Phone />
-          <Email />
-        </div>
-      </div>
-    </nav>
-  );
+	);
 };
