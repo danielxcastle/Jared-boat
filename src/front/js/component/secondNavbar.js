5881d872ccfbeab44d148b74a711a24c8985efaa
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2024-02-01 04:02:10 +0000

    trying new

--- src/front/js/component/secondNavbar.js
+++ src/front/js/component/secondNavbar.js
@@ -56,11 +56,11 @@ export const SecondNavbar = () => {
               </h5>
             </div>
           </div>
+          <Phone />
+          <BookingEmail />
           <a href="https://www.instagram.com/capt_jared_m/" target="_blank" rel="noopener noreferrer" className="lower-nav-btn-insta text-black text-center">
             <h5><i className="fa-brands fa-instagram insta-logo"></i></h5>
           </a>
-          <Phone />
-          <BookingEmail />
           </div>
       </div>
     </nav>
